574ff723b50ba12ed02939765385501266eaecbe
Jasur Tohirov <Jasur Tohirov>
2025-10-15 21:29:23 +0500

    Login qushildi va dizayinda uzgarishlar buldi

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'car_list'
+LOGOUT_REDIRECT_URL = 'car_list'
